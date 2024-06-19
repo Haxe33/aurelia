c4f9306e5a146a20971e05e92208ec225287327f
laulaobm <131969052+laulaobm@users.noreply.github.com>
2024-06-19 10:59:52 +0200

    updated description

--- app/src/main/java/com/example/aurelia/Description.kt
+++ app/src/main/java/com/example/aurelia/Description.kt
@@ -8,17 +8,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
@@ -26,6 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
+import com.example.aurelia.ui.theme.AureliaTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
@@ -40,13 +43,81 @@ class Description : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-           ZodiacSignSwiper(
-           )
+            AureliaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ZodiacSignSwiper(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
     }
+    /*@OptIn(ExperimentalPagerApi::class)
+    @Composable
+    fun TopBarSwiper(modifier: Modifier){
+        var selectedTabIndex by remember {
+            mutableIntStateOf(0)
+        }
+        val textSlider=listOf(
+            Text(text = "Beschreibung"),
+            Text(text = "Horoskop"),
+            Text(text = "Aszendent"),
+            Text(text = "Compatibility Check")
+        )
+        val pagerState = rememberPagerState {
+            textSlider.size
+        }
+
+        LaunchedEffect(key1 = selectedTabIndex) {
+            pagerState.animateScrollToPage(selectedTabIndex)
+        }
+
+        LaunchedEffect(key1 = pagerState.currentPage, pagerState.isScrollInProgress) {
+            if (!pagerState.isScrollInProgress)
+                selectedTabIndex = pagerState.currentPage
+        }
+
+        Column {
+            HorizontalPager(
+                count = textSlider.size,
+                state = pagerState,
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                modifier = Modifier
+                    .height(170.dp)
+                    .fillMaxWidth()
+            ) { page ->
+                Card(
+                    modifier = modifier
+                        .graphicsLayer {
+                            val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                            lerp(
+                                start = 0.85f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            ).also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+
+                            alpha = lerp(
+                                start = 0.5f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            )
+                        }
+                ){
+                    val string: String= textSlider[page].toString()
+                    Text(string)
+                }
+            }
+        }
+
+    }*/
+    //Quelle: [L1]
     @OptIn(ExperimentalPagerApi::class)
     @Composable
-    fun ZodiacSignSwiper(){
+    fun ZodiacSignSwiper(modifier: Modifier){
         val pagerState = rememberPagerState(initialPage = 0)
         val imageSlider = listOf(
             painterResource(id = R.drawable.steinbock),
@@ -77,12 +148,12 @@ class Description : ComponentActivity() {
                 state = pagerState,
                 contentPadding = PaddingValues(horizontal = 16.dp),
                 modifier = Modifier
-                    .height(114.dp)
+                    .height(170.dp)
                     .fillMaxWidth()
             ) { page ->
                 Card(
                     shape = RoundedCornerShape(12.dp),
-                    modifier = Modifier
+                    modifier = modifier
                         .graphicsLayer {
                             val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
 
@@ -110,13 +181,6 @@ class Description : ComponentActivity() {
                     )
                 }
             }
-
-            HorizontalPagerIndicator(
-                pagerState = pagerState,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(16.dp)
-            )
         }
     }
 
@@ -124,7 +188,9 @@ class Description : ComponentActivity() {
     @Composable
     fun DefaultPreview() {
         MaterialTheme {
-            ZodiacSignSwiper()
+            ZodiacSignSwiper(Modifier.padding(vertical = 15.dp))
+            Spacer(modifier = Modifier.height(40.dp))
+            //TopBarSwiper(modifier = Modifier)
         }
     }
 }
