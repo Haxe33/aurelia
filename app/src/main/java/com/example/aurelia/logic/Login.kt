615187bc5348cab17cae66be6f63e03b70f2f95c
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-07-25 16:35:15 +0200

    feat: finalized Loginpage

--- app/src/main/java/com/example/aurelia/logic/Login.kt
+++ app/src/main/java/com/example/aurelia/logic/Login.kt
@@ -7,20 +7,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.example.aurelia.MainActivity
 
-
-
-fun handleLogin(pwd:String, user: String): Boolean{
-    return checkPassword(pwd, user)
+fun handleLogin(pwd:String, user: String, context: Context): Boolean{
+    return checkPassword(pwd, user, context)
 }
 
-fun handleRegistration(pwd: String, user: String){
-
+fun handleRegistration(pwd: String, user: String, zodiac: String, context: Context){
+    val str = "$user,$pwd,$zodiac\n"
+    writeUserData(context, str)
 }
-fun checkPassword(pwd: String, username: String): Boolean{
-    val users = readUserData()
-    for(user in users){
-        if(user.getUser() == username){
-            if(user.getPassword() == pwd){
+
+fun checkUsername(context: Context, user: String): Boolean{
+    val users = readUserData(context)
+    users.forEach {
+        if(it.getUser() == user){
+            return false
+        }
+    }
+    return true
+}
+fun checkPassword(pwd: String, username: String, context: Context): Boolean{
+    val users = readUserData(context)
+    users.forEach{
+        if(it.getUser() == username){
+            if(it.getPassword() == pwd){
                 return true
             }
         }
