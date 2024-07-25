26d16528dc597cbec457c15c1c3afb86ada13c2b
paulxyh <77801088+paulxyh@users.noreply.github.com>
2024-07-25 16:35:24 +0200

    feat: Added NavigationGraph

---
+++ app/src/main/java/com/example/aurelia/Navigation.kt
@@ -0,0 +1,16 @@
+package com.example.aurelia
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(){
+    val controller = rememberNavController()
+    NavHost(navController = controller, startDestination = "login"){
+        composable("login"){ Greeting(controller) }
+        composable("registration"){Registration(controller)}
+        composable("description"){}
+    }
+}
