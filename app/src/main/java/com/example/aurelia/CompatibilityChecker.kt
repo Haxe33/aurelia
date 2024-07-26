6b7b9a640a324353d215fecbd88c31b2133a2c2f
laulaobm <laura@diehofers.at>
2024-07-26 16:26:32 +0200

    first version of compatibilityChecker
    still pseudo texts and percentages

--- app/src/main/java/com/example/aurelia/CompatibilityChecker.kt
+++ app/src/main/java/com/example/aurelia/CompatibilityChecker.kt
@@ -1,6 +1,7 @@
 package com.example.aurelia
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,22 +10,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aurelia.logic.ZodiacSign
 import com.example.aurelia.ui.theme.*
+import kotlinx.coroutines.delay
 
 @Composable
 fun CompatibilityCheckerScreen(currentZodiacSign: ZodiacSign) {
     val scrollState = rememberScrollState()
     Column(modifier = Modifier
         .verticalScroll(scrollState)
-        .padding(15.dp)) {
+        .padding(15.dp))
+    {
         Heading(currentZodiacSign.name)
         Spacer(Modifier.height(8.dp))
         //TODO add Text wie das Sternzeichen so in der Liebe ist
@@ -34,29 +47,76 @@ fun CompatibilityCheckerScreen(currentZodiacSign: ZodiacSign) {
         Spacer(Modifier.height(6.dp))
         val compatibleSign=zodiacSignSwiper(modifier = Modifier.fillMaxWidth())
         Heading(compatibleSign.name)
-        Spacer(Modifier.height(6.dp))
-
-
-
+        Spacer(Modifier.height(8.dp))
+        //TODO add Text wie das Sternzeichen so in der Liebe ist
+        Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+        Spacer(Modifier.height(8.dp))
+        PinchReveal(modifier = Modifier.size(240.dp))
+        Instructions(str = "Press long on the heart to reveal your soulmate!")
     }
 
 }
 
-//Quelle [L4]
+//Quelle [L4] for gesture handling
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun PinchReveal(imageRes: Int, modifier: Modifier = Modifier){
-    Box( modifier = Modifier
-        .fillMaxSize()
-        .padding(10.dp)
-        .combinedClickable(
-            enabled = true,
-            onLongClick = {
-                //TODO change imagesource
-            },
-            onClick = {}
-        )
+fun PinchReveal(modifier: Modifier = Modifier) {
+    var isVisible by remember { mutableStateOf(true) }
+    var progress by remember { mutableStateOf(0.0f) }
+    val targetProgress= getCompatibility()
+
+    Column(
+        modifier=Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
-      //TODO image
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(10.dp)
+                .combinedClickable(
+                    enabled = true,
+                    onLongClick = {
+                        isVisible = !isVisible
+                    },
+                    onClick = {}
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            if(isVisible) {
+                Image(
+                    painter = painterResource(id = R.drawable.heart),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }else{
+                LaunchedEffect(isVisible) {
+                    progress=0.0f
+                    while(progress<targetProgress){
+                        progress += 0.01f
+                        delay(10)
+                    }
+                    progress=targetProgress
+                }
+                CircularProgressIndicator(
+                    progress = { progress },
+                    modifier = Modifier.size(190.dp),
+                    color = Color(0xffd15b8f),
+                    strokeWidth = 15.dp,
+                )
+                Text(
+                    text = "${(progress * 100).toInt()}% match",
+                    color = Color(0xffd15b8f),
+                    fontSize = 27.sp,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+        }
     }
+}
+
+//zwei ZodiacSigns übergeben
+fun getCompatibility(): Float {
+    //TODO manage compatibilities
+    return 0.75f
 }
--- app/src/main/java/com/example/aurelia/Description.kt
+++ app/src/main/java/com/example/aurelia/Description.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TabRowDefaults.SecondaryIndicator
 import androidx.compose.runtime.Composable
@@ -31,22 +27,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.lerp
 import com.example.aurelia.logic.ZodiacSign
 import com.example.aurelia.ui.theme.AureliaTheme
 import com.example.aurelia.ui.theme.Heading
 import com.example.aurelia.ui.theme.zodiacSignSwiper
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.google.accompanist.pager.rememberPagerState
-import kotlin.math.absoluteValue
 
 class Description : ComponentActivity() {
 
@@ -70,7 +60,7 @@ class Description : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AureliaTheme {
-                //Quelle[L2]
+                //Quelle[L2] for mutable structure
                 var currentZodiacSign by remember { mutableStateOf(zodiacSigns[0]) }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)) {
@@ -81,7 +71,7 @@ class Description : ComponentActivity() {
             }
         }
     }
-    //Quelle[L3]
+    //Quelle[L3] for swiper logic
     @OptIn(ExperimentalPagerApi::class)
     @Composable
     fun TopBarSwiper(currentZodiacSign: ZodiacSign) {
@@ -170,6 +160,7 @@ class Description : ComponentActivity() {
             Text(
                 "DESCRIPTION Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet\n"
             )
+            //TODO add description
         }
     }
 
